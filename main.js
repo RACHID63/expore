6696645a5eac76c7f2e3348a190fa39cb1d20768
LAIEB Rachid <rachid.laieb@gmail.com>
2023-03-15 09:02:31 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+import add from './mathFonction.js'
+
+
+add(2, 25);
+// sub(10, 11);
+
+
+
+
+
+
+
---
+++ mathFonction.js
@@ -0,0 +1,7 @@
+export const add = (x, y) => console.log(x + y);
+export const sub = (x, y) => console.log(x - y);
+
+
+
+
+
